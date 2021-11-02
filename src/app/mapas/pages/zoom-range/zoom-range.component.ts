cca87c48f00c9b0d049f5ac6adf4f2c628e898fb
MiguelMuros <miggonzalv@gmail.com>
2021-11-02 20:27:31 +0100

    feat: create listeners for zoom range element

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -6,30 +6,66 @@ import * as mapboxgl from 'mapbox-gl';
   templateUrl: './zoom-range.component.html',
   styles: [`
     .mapa-container { width:100%; height: 100%; }
-    .row { background-color: white; border-radius: 5px; padding:10px; bottom:50px; left: 50px; position:fixed; z-index: 999; }
+    .row { background-color: white; border-radius: 5px; padding:10px; bottom:50px; left: 50px; width:400px; position:fixed; z-index: 999; }
   `]
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('mapa') divMapa!: ElementRef;
   mapa!: mapboxgl.Map;
+  zoomLevel: number = 10;
+  center: [number, number] = [-6.107683786860004, 43.542147667593234]
 
   constructor() { }
 
+  ngOnDestroy(): void {
+    // En el destroy debemos limpiar todos los listeners creados
+    this.mapa.off('zoom', () => { });
+    this.mapa.off('zoomend', () => { });
+    this.mapa.off('moved', () => { });
+  }
+
   ngAfterViewInit(): void {
+
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-6.107683786860004, 43.542147667593234],
-      zoom: 17
+      center: this.center,
+      zoom: this.zoomLevel
     });
+
+    this.mapa.on('zoom', () => {
+      // Asignamos el valor de zoom que tenemos en cada momento
+      this.zoomLevel = this.mapa.getZoom();
+    });
+
+    this.mapa.on('zoomend', () => {
+      // El zoom asignado no debe ser mayor a 18
+      if (this.mapa.getZoom() > 18) {
+        this.mapa.zoomTo(18);
+      }
+    });
+
+    this.mapa.on('move', (event) => {
+      // Asignamos las nuevas coordenadas cuando el usuario mueve el cursor de lugar
+      const { lng, lat } = event.target.getCenter();
+      this.center = [lng, lat]
+    });
+
   }
 
   zoomOut(): void {
     this.mapa.zoomOut();
+    this.zoomLevel = this.mapa.getZoom();
   }
 
   zoomIn(): void {
     this.mapa.zoomIn();
+    this.zoomLevel = this.mapa.getZoom();
   }
+
+  zoomCambio(valor: string): void {
+    this.mapa.zoomTo(Number(valor));
+  }
+
 }
