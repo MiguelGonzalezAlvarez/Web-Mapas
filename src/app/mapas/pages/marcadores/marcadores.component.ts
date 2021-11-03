caaa556ffd94ca1da46a7cf163f39bd7e8dd1491
MiguelMuros <miggonzalv@gmail.com>
2021-11-03 20:22:14 +0100

    feat: create marcadores screen and design all the map logic to add markers

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,124 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+interface MarcadorColor {
+  color: string;
+  marker?: mapboxgl.Marker;
+  centro?: [number, number]
+}
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
+    `
+    .mapa-container {
+      height: 100%;
+      width: 100%; 
+    }
+
+    .list-group {
+      position: fixed;
+      top: 20px;
+      right: 20px;
+      z-index: 99;
+    }
+
+    li {
+      cursor: pointer;
+    }`
   ]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+  @ViewChild('mapa') divMapa!: ElementRef;
 
-  constructor() { }
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  center: [number, number] = [-75.921029433568, 45.28719674822362];
+  marcadores: MarcadorColor[] = [];
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel
+    });
+
+    this.leerLocalStorage();
+
+    // Forma de poner marcadores de forma fija
+    // Descomentar en caso de no querer crear dinamicamente los marcadores
+    // this.crearMarcadorFijo();
+
   }
+
+  crearMarcadorFijo(): void {
+    const markerHtml: HTMLElement = document.createElement('div');
+    markerHtml.innerHTML = 'Hola Mundo';
+
+    // Ponemos un marcador en el centro del mapa
+    new mapboxgl.Marker()
+      .setLngLat(this.center)
+      .addTo(this.mapa);
+  }
+
+
+  agregarMarcador() {
+    const color = "#xxxxxx".replace(/x/g, y => (Math.random() * 16 | 0).toString(16));
+    const nuevoMarcador = new mapboxgl.Marker({ draggable: true, color }).setLngLat(this.center).addTo(this.mapa);
+
+    this.marcadores.push({ color, marker: nuevoMarcador });
+    this.guardarMarcadoresLocalStorage()
+
+    nuevoMarcador.on('dragend', () => {
+      this.guardarMarcadoresLocalStorage();
+    });
+  }
+
+  irMarcador(marker: mapboxgl.Marker) {
+    this.mapa.flyTo({
+      center: marker.getLngLat()
+    });
+  }
+
+
+  guardarMarcadoresLocalStorage() {
+    const lngLatArr: MarcadorColor[] = [];
+
+    this.marcadores.forEach(marcador => {
+      const color = marcador.color;
+      const { lng, lat } = marcador.marker!.getLngLat();
+      lngLatArr.push({ color: color, centro: [lng, lat] });
+    })
+
+    localStorage.setItem('marcadores', JSON.stringify(lngLatArr));
+  }
+
+  leerLocalStorage() {
+    if (!localStorage.getItem('marcadores')) {
+      // Si no se han guardado marcadores entonces no hacemos nada
+      return;
+    }
+
+    const lngLatArr: MarcadorColor[] = JSON.parse(localStorage.getItem('marcadores')!);
+
+    lngLatArr.forEach(m => {
+      const newMarker = new mapboxgl.Marker({ color: m.color, draggable: true }).setLngLat(m.centro!).addTo(this.mapa);
+
+      this.marcadores.push({
+        marker: newMarker,
+        color: m.color
+      });
+
+      newMarker.on('dragend', () => {
+        this.guardarMarcadoresLocalStorage();
+      });
+
+    });
+
+  }
+
+
 
 }
